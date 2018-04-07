90b7286e60ffd63d40ba02c692de55fb3a1a8488
Andrew Szeto <andrew@jabagawee.com>
2018-04-06 20:36:50 -0700

    Extract Alarms class into its own file

--- AlarmClockControl/src/alarmclock.py
+++ AlarmClockControl/src/alarmclock.py
@@ -22,14 +22,14 @@ import textwrap
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
 
-from crontab import CronTab
-
 from twisted.internet import reactor
 from twisted.internet import task
 from twisted.internet.serialport import SerialPort
 from twisted.protocols.basic import LineReceiver
 from twisted.web import server
 from twisted.web.server import resource
+
+from .alarms import Alarms
 
 __all__ = []
 __version__ = 0.1
@@ -79,81 +79,6 @@ def stopPlaying(client):
 def mpdClose(client):
     client.close()
     client.disconnect()
-
-
-class Alarm(object):
-    '''A single (possibly recurring) alarm.'''
-
-    def __init__(self, crontab):
-        self._crontab = crontab
-        self._alarm = CronTab(crontab)
-
-    def next(self, now=None):
-        if now is None:
-            return self._alarm.next(default_utc=False)
-        else:
-            return self._alarm.next(now=now, default_utc=False)
-
-    def get_crontab(self):
-        return self._crontab
-
-
-class Alarms(object):
-    '''Container for multiple recurring alarms.'''
-
-    def __init__(self, save_path):
-        self._save_path = None
-        self._alarms = []
-        if save_path != '':
-            self._save_path = save_path
-            try:
-                with open(self._save_path) as f:
-                    self._alarms = [Alarm(line.strip())
-                                    for line in f.readlines()
-                                    if not line.startswith('#')]
-            except Exception:
-                sys.stdout.write('Ignoring missing save file: %s\n' % self._save_path)
-            else:
-                sys.stdout.write('Loaded %s alarms from save file %s:\n  %s\n' %
-                                 (len(self._alarms), self._save_path,
-                                  '\n  '.join(alarm.get_crontab() for alarm in self._alarms)))
-
-    def reschedule_all(self, alarms):
-        sys.stdout.write('Rescheduling new alarms:\n  %s\n' %
-                         ('\n  '.join(alarm.strip() for alarm in alarms),))
-        self._alarms = [Alarm(alarm.strip()) for alarm in alarms]
-        if self._save_path:
-            try:
-                with open(self._save_path, 'w') as f:
-                    f.write('# m h dom mon dow\n')
-                    for alarm in self._alarms:
-                        f.write(alarm.get_crontab() + '\n')
-                sys.stdout.write('Wrote %s alarms to save file %s\n' %
-                                 (len(self._alarms), self._save_path))
-            except Exception:
-                sys.stderr.write('Failed to write to save file: %s\n' % self._save_path)
-
-    def next_alarm(self):
-        return min([alarm.next() for alarm in self._alarms])
-
-    def next_alarms(self, num_alarms, now=None):
-        if now is None:
-            now = datetime.datetime.now()
-        result = []
-
-        for _ in range(num_alarms):
-            now += datetime.timedelta(seconds=min(
-                alarm.next(now=now)
-                for alarm in self._alarms))
-            result.append(now)
-
-        return result
-
-    def num_alarms(self):
-        return len(self._alarms)
-
-    def get_alarm_crontabs(self):
-        return [alarm.get_crontab() for alarm in self._alarms]
 
 
 class SerialProtocol(LineReceiver):
---
+++ AlarmClockControl/src/alarms.py
@@ -0,0 +1,78 @@
+import datetime
+import sys
+
+from crontab import CronTab
+
+class Alarm(object):
+    '''A single (possibly recurring) alarm.'''
+
+    def __init__(self, crontab):
+        self._crontab = crontab
+        self._alarm = CronTab(crontab)
+
+    def next(self, now=None):
+        if now is None:
+            return self._alarm.next(default_utc=False)
+        else:
+            return self._alarm.next(now=now, default_utc=False)
+
+    def get_crontab(self):
+        return self._crontab
+
+
+class Alarms(object):
+    '''Container for multiple recurring alarms.'''
+
+    def __init__(self, save_path):
+        self._save_path = None
+        self._alarms = []
+        if save_path != '':
+            self._save_path = save_path
+            try:
+                with open(self._save_path) as f:
+                    self._alarms = [Alarm(line.strip())
+                                    for line in f.readlines()
+                                    if not line.startswith('#')]
+            except Exception:
+                sys.stdout.write('Ignoring missing save file: %s\n' % self._save_path)
+            else:
+                sys.stdout.write('Loaded %s alarms from save file %s:\n  %s\n' %
+                                 (len(self._alarms), self._save_path,
+                                  '\n  '.join(alarm.get_crontab() for alarm in self._alarms)))
+
+    def reschedule_all(self, alarms):
+        sys.stdout.write('Rescheduling new alarms:\n  %s\n' %
+                         ('\n  '.join(alarm.strip() for alarm in alarms),))
+        self._alarms = [Alarm(alarm.strip()) for alarm in alarms]
+        if self._save_path:
+            try:
+                with open(self._save_path, 'w') as f:
+                    f.write('# m h dom mon dow\n')
+                    for alarm in self._alarms:
+                        f.write(alarm.get_crontab() + '\n')
+                sys.stdout.write('Wrote %s alarms to save file %s\n' %
+                                 (len(self._alarms), self._save_path))
+            except Exception:
+                sys.stderr.write('Failed to write to save file: %s\n' % self._save_path)
+
+    def next_alarm(self):
+        return min([alarm.next() for alarm in self._alarms])
+
+    def next_alarms(self, num_alarms, now=None):
+        if now is None:
+            now = datetime.datetime.now()
+        result = []
+
+        for _ in range(num_alarms):
+            now += datetime.timedelta(seconds=min(
+                alarm.next(now=now)
+                for alarm in self._alarms))
+            result.append(now)
+
+        return result
+
+    def num_alarms(self):
+        return len(self._alarms)
+
+    def get_alarm_crontabs(self):
+        return [alarm.get_crontab() for alarm in self._alarms]
